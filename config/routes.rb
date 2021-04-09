6144ecc08f0ecd92663255bcec217b7909dfc6c8
takumi20210430 <uniquetime.t@gmail.com>
2021-04-09 12:36:52 +0000

    [Finish]4-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  
   root 'post_images#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
     
     resources :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show, :edit, :update]
+  
   
 end
